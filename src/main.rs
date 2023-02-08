e01adae688fcd724b05ac48dda0a59cbba2cfc7b
Dmitry Pronin <dpronin@yandex.ru>
2023-02-07 21:21:27 -0800

    create config file to store ..... configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use clap::Parser;
 use colored::{ColoredString, Colorize};
 use std::{fs, path::PathBuf};
+extern crate nerd_fonts;
+use nerd_fonts::NerdFonts;
+use serde_derive::Deserialize;
+use std::process::exit;
+use toml;
+
+// Top level struct to hold the TOML data.
+#[derive(Deserialize)]
+struct Data {
+    config: Config,
+}
+
+// Config struct holds to data from the `[config]` section.
+#[derive(Deserialize)]
+struct Config {
+    ip: String,
+    port: u16,
+}
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -14,6 +32,26 @@ struct Args {
 }
 
 fn main() {
+    let filename = "config.toml";
+    let contents = match fs::read_to_string(filename) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read file `{}`", filename);
+            exit(1);
+        }
+    };
+
+    let data: Data = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Unable to load data from `{}`", filename);
+            exit(1);
+        }
+    };
+
+    // Print out the values to `stdout`.
+    println!("{}", data.config.ip); // => 42.69.42.0
+    println!("{}", data.config.port); // => 42
     let args = Args::parse();
     let path = args.path;
     get_files(&path)
@@ -23,17 +61,20 @@ fn get_files(path: &String) {
     for entry in fs::read_dir(path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
-
-        println!("{}", path.color());
+        let nf = NerdFonts {
+            nf: NerdFonts::load(),
+        };
+        let nf_custom_c = nf.get("fa-folder_open_o").unwrap(); // '\u{e61e}'
+        println!("{} {}", nf_custom_c, path.prinbtable());
     }
 }
 
 trait Coloring {
-    fn color(&self) -> ColoredString;
+    fn prinbtable(&self) -> ColoredString;
 }
 
 impl Coloring for PathBuf {
-    fn color(&self) -> ColoredString {
+    fn prinbtable(&self) -> ColoredString {
         let path = &self.to_str().unwrap();
         if self.is_relative() {
             return path.green();
